2d2e7416de1951ce8742e54d02429a25a5be8e13
phanuell-365 <phanuelkidakwa@gmail.com>
2022-11-10 12:17:43 +0300

    Added users and bookmarks module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { BookmarksModule } from './bookmarks/bookmarks.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, BookmarksModule],
   controllers: [AppController],
   providers: [AppService],
 })
