74e22f1773869be2eea1bb99b345ec9848509f0e
phanuell-365 <phanuelkidakwa@gmail.com>
2022-11-10 12:23:49 +0300

    Added auth and database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
+import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, BookmarksModule],
+  imports: [UsersModule, BookmarksModule, DatabaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
